ae1230d24884c376258a8a94a97d350e32266d45
xujun <xujun1118@gmail.com>
2018-03-11 14:06:11 +0800

    first commit!

---
+++ mall/src/main/java/com/mall/mall/App.java
@@ -0,0 +1,14 @@
+package com.mall.mall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("first project");
+    }
+}
